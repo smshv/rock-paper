86ced24429dd58dfa35ef3267e5bbc706c2659fa
shohaibMahmud <shohaib.mahmud@gmail.com>
2023-06-15 22:22:49 -0400

    Add graphical user interface

--- game.js
+++ game.js
@@ -1,46 +1,93 @@
 let choices = ["Rock", "Paper", "Scissor"];
+let lastPlayerChoice = null,
+    lastComputerChoice = null;
 
-function inputFormat(input){
-    return input.charAt(0).toUpperCase()+input.slice(1, input.length).toLowerCase();
-}
+let screenText = {
+    "round-left" : 5,
+    "player-score" : 0,
+    "computer-score" : 0,
+    "round-msg" : "",
+};
+const scrTextElems = document.querySelectorAll(".scr-text");
 
 function getComputerChoice(){
     return choices[Math.floor(Math.random()*10)%3];
 }
 
-function playRound(playerSelection, computerSelection){
+function updateScreenText(){
+    scrTextElems.forEach(
+      elem=>{
+        elem.textContent = `${screenText[elem.classList[1]]}`;
+      }  
+    );
+    
+}
+
+function reTransformButton(){ //remove transfrom of the last selected button
+    if (lastPlayerChoice){
+        lastPlayerChoice.classList.remove("selected");
+        lastComputerChoice.classList.remove("selected");
+    }
+}
+
+function transformButton(playerChoice, computerChoice){
+}
+
+function initalize(){
+    screenText["round-left"] = 5;
+    screenText["player-score"] = 0;
+    screenText["computer-score"] = 0;
+    screenText["round-msg"] = "No round has been played yet";
+    reTransformButton();
+    lastPlayerChoice = null,
+    lastComputerChoice = null;
+    updateScreenText();
+}
+function decideWinner(playerSelection, computerSelection){
     if (playerSelection==computerSelection){
-        console.log("Round ties!");
-        return [0, 0];
+        screenText["round-msg"] = "Round ties";
     }
     else if ((playerSelection=="Rock")&&(computerSelection=="Scissor")||
     (playerSelection=="Scissor")&&(computerSelection=="Paper")||
     (playerSelection=="Paper")&&(computerSelection=="Rock")){
-        console.log(`You win this round!${playerSelection} beats ${computerSelection}`);
-        return [1, 0];
+        screenText["round-msg"] = `You win this round! ${playerSelection} beats ${computerSelection}`;
+        screenText["player-score"] += 1;
     }
     else{
-        console.log(`You lose this round!${computerSelection} beats ${playerSelection}`);
-        return [0, 1];
+        screenText["round-msg"] = `You lose this round :( ${computerSelection} beats ${playerSelection}`;
+        screenText["computer-score"] += 1;
     }
 }
-
-function game(){
-    let playerScore, computerScore;
-    let playerScoreTotal = 0,
-        computerScoreTotal = 0;
-    for (let i=0; i<5; i++){
-        [playerScore, computerScore] = playRound(inputFormat(prompt("Input your move")), getComputerChoice());
-        playerScoreTotal += playerScore;
-        computerScoreTotal += computerScore;
-
+function playRound(e){
+    if (!screenText["round-left"]) initalize();
+    const playerSelection = e.currentTarget.id;
+    const computerSelection = getComputerChoice();
+    const computerButton = document.querySelector(`.computer-button > #${computerSelection}`);
+    decideWinner(playerSelection, computerSelection);
+    reTransformButton();
+    e.currentTarget.classList.add("selected");
+    computerButton.classList.add("selected");
+    lastPlayerChoice = e.currentTarget;
+    lastComputerChoice = computerButton;
+    screenText["round-left"] -= 1;
+    if (screenText["round-left"]==0){
+        if (screenText["player-score"]==screenText["computer-score"])
+            screenText["round-msg"] = "Game ties";
+        else if(screenText["player-score"]>screenText["computer-score"])
+            screenText["round-msg"] = "You win!";
+        else
+            screenText["round-msg"] = "You lose :(";
     }
-     
-    if(playerScoreTotal>computerScoreTotal){
-        console.log("You win!");
-    }
-    else{
-        console.log("Computer wins!");
-    }
+    updateScreenText();
+    e.stopPropagation();
 }
-game();
+
+initalize();
+
+document.querySelectorAll(".player-button > button").
+forEach(button=>{
+    button.addEventListener("click", playRound);
+});
+
+document.querySelector(".reset-div > button").
+addEventListener("click", initalize);
